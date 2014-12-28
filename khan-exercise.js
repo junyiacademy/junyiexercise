621016d4d49d3a437587c1b93f5e0daf9fe23124
Ins Huang <ins.huang@gmail.com>
2014-12-28 18:07:08 +0800

    Update from junyiacademy
    - A bug fix by EN, fix the issue of focus() called after show().

--- khan-exercise.js
+++ khan-exercise.js
@@ -2124,8 +2124,9 @@ var Khan = (function() {
                     $("#next-question-button")
                         .removeAttr("disabled")
                         .removeClass("buttonDisabled")
-                        .show()
-                        .focus();
+                        .show(10,function(){
+                            $(this).focus();
+                        });
                     $("#positive-reinforcement").show();
                 }
             } else {
