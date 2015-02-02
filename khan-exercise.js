bcfe37602cf432c0b4e51b252d91f258530178b8
RubyLiou <shiny0066@gmail.com>
2015-02-02 09:45:46 +0800

    no message

--- khan-exercise.js
+++ khan-exercise.js
@@ -1018,7 +1018,8 @@ var Khan = (function() {
         var cached = LocalStore.get(cacheKey);
         var lastProblemNum = (cached && cached["lastProblemNum"]) || 0;
 
-        if (lastProblemNum === problemNum && !userExercise.exerciseModel.isQuizExercise) {
+        // check new_problem_bag to know which A/B test alternative is.
+        if (lastProblemNum === problemNum && !userExercise.exerciseModel.isQuizExercise && new_problem_bag) {
             // Getting here means the user refreshed the page or returned to
             // this exercise after being away. So, we don't need to and
             // shouldn't skip this problem.
@@ -1032,7 +1033,8 @@ var Khan = (function() {
         // few problems, but not if we've been fruitlessly skipping for a while.
         // The latter situation could happen if a problem has very few unique
         // problems (eg. exterior angles problem type of angles_of_a_polygon).
-        if (userExercise.exerciseModel.isQuizExercise){
+        // check new_problem_bag to know which A/B test alternative is.
+        if (userExercise.exerciseModel.isQuizExercise && new_problem_bag){
             dupWindowSize = 9;
         }else{
             dupWindowSize = 5;
