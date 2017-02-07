5bec170189b6812bcfd873a3301915912e64f72c
40240306s <40240306s@ntnu.edu.tw>
2017-02-07 11:02:43 +0800

    問題:部分題目的答案按鈕無反應或功能不正常
    修正:將音效元件觸發的部分加入null判斷

--- interface.js
+++ interface.js
@@ -288,7 +288,9 @@ function handleAttempt(data) {
         $("#check-answer-results > p").hide();
     } else if (score.correct) {
         // Correct answer, so show the next question button.
-        document.getElementById("au-correct-answer").play();
+        if (document.getElementById("au-correct-answer") !== null) {
+            document.getElementById("au-correct-answer").play();
+        }
         $("#check-answer-button").hide();
         $("#check-answer-results > p").hide();
         $("#next-question-button")
@@ -314,7 +316,9 @@ function handleAttempt(data) {
         $("#skip-question-button").prop("disabled", true);
     } else {
         // Wrong answer. Enable all the input elements
-        document.getElementById("au-wrong-answer").play();
+        if (document.getElementById("au-wrong-answer") !== null) {    
+            document.getElementById("au-wrong-answer").play();
+        }
         $("#check-answer-button")
             .val($._("答錯了，再試試看喔！"))
             .parent()  // .check-answer-wrapper makes shake behave
