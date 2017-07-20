8491320f60971ecd819df2ee12d8c2db3bfd2fd8
Kevin Lin <kevinptt0323@gmail.com>
2017-07-20 18:43:00 +0800

    fix incorrect selector to shake

--- interface.js
+++ interface.js
@@ -28,7 +28,7 @@ _.extend(Exercises, {
     guessLog: undefined,
     userActivityLog: undefined
 });
-var hintCanVibration = true; 
+var hintCanVibration = true;
 var waitForVibration = false;
 var firstTimeWrong = true;
 var PerseusBridge = Exercises.PerseusBridge,
@@ -330,8 +330,8 @@ function handleAttempt(data) {
                 setTimeout(function() {
                     waitForVibration = false;
                     hintCanVibration = true;
-                },1500);               
-        } 
+                },1500);
+        }
 
         // Is this a message to be shown?
         if (score.message != null) {
@@ -464,7 +464,7 @@ function onHintShown(e, data) {
     if (hintsUsed === numHints) {
         $("#hint").attr("disabled", true);
         $(Exercises).trigger("allHintsUsed");
-       
+
         if (userExercise.exerciseStates.struggling) {
             if (hintCanVibration === true) {
                 hintCanVibration = false;
@@ -623,7 +623,7 @@ function request(method, data) {
                     if (hint_disabled === "disabled") {
                         if (hintCanVibration === true) {
                             hintCanVibration = false;
-                            $("#raise-hand-button").effect("shake", {times: 3, distance: 5}, 480);
+                            $("#raise-hand-button-container").effect("shake", {times: 3, distance: 5}, 480);
                         }
                     }
                     else {
